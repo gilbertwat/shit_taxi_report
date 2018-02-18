52a72b6aff2bb4a3d7303428912c6109c48cd958
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2018-02-18 12:51:54 +0800

    Completed create report flow sans UX

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,30 +1,95 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <form v-on:submit.prevent>
+      <fieldset>
+        <button type="button" v-on:click="allowLocation">迅速送出位置先</button>
+      </fieldset>
+      <fieldset>
+        <label for="license_plate">
+          條粉腸個車牌:
+        </label>
+        <input id="license_plate" name="license_plate" v-model="license_plate">
+      </fieldset>
+      <fieldset>
+        <label for="type-rejected">又拒載我dllm:</label>
+        <input id="type-rejected" type="radio" v-model="type" name="type" value="rejected">
+        <label for="type-too-much-fare">又借啲E收多:</label>
+        <input id="type-too-much-fare" type="radio" v-model="type" name="type" value="too_much_fare">
+      </fieldset>
+      <fieldset>
+        <template v-if="isLocationReady()">
+          <div id="mapid">
+            <v-map :zoom=13 :center="[47.413220, -1.219482]">
+              <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+              <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+            </v-map>
+          </div>
+        </template>
+      </fieldset>
+      <fieldset>
+        <label for="lat">lat:</label>
+        <input id="lat" type="number" v-model.number="lat" name="lat" step="0.000001">
+      </fieldset>
+      <fieldset>
+        <label for="lon">lon:</label>
+        <input id="lon" type="number" v-model.number="lon" name="lon" step="0.000001">
+      </fieldset>
+      <button v-bind:disabled="isLocationReady()" type="button" v-on:click="dllm">屌你老母</button>
+    </form>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      license_plate: "",
+      type: "rejected",
+      lat: NaN,
+      lon: NaN,
+      accuracy: NaN
+    }
+  },
+  methods: {
+    isLocationReady() {
+      return !(isNaN(this.lat) || isNaN(this.lon));
+    },
+    showLoading() {
+      console.log("攞緊喇知你燥")
+    },
+    hideLoading() {
+      console.log("攞到喇催催催")
+    },
+    allowLocation() {
+      this.showLoading();
+      if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          this.hideLoading();
+          console.log(this);
+          console.log(pos);
+          this.lat = pos.coords.latitude;
+          this.lon = pos.coords.longitude;
+          this.accuracy = pos.coords.accuracy;
+        });
+      }
+    },
+    dllm: function() {
+      //validate(this);
+      console.log(this);
+      axios.post('./api/reports', {
+        shit_taxi_report: {
+          lat: this.lat,
+          lon: this.lon,
+          license_plate: this.license_plate,
+          type: this.type
+        }
+      }).then((response) => {
+
+      }).catch((error) =>{
+
+      })
     }
   }
 }
@@ -38,6 +103,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#mapid { height: 180px; }
+
+label {
+  font-size:2.3333rem;
 }
 
 h1, h2 {
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, '../priv/static/js'),
-    publicPath: '../priv/static/js',
+    path: path.resolve(__dirname, '../priv/static/js/'),
+    publicPath: 'js/',
     filename: 'app.js'
   },
   module: {
